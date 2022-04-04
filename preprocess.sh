a2cf6d1f2f393345306708b413feea6da94c20a9
Connor Makowski <connorman528@gmail.com>
2022-04-04 09:26:47 -0400

    Fix preprocessing step to include common edX imports

--- preprocess.sh
+++ preprocess.sh
@@ -6,7 +6,9 @@ do
   do
     rm -r "./$folder/python/$(basename $xmlfile .xml).py" &> /dev/null
     touch "./$folder/python/$(basename $xmlfile .xml).py"
-    echo "import math, random, scipy, numpy" > "./$folder/python/$(basename $xmlfile .xml).py"
+    echo "import math, scipy, numpy" > "./$folder/python/$(basename $xmlfile .xml).py"
+    echo "import random2 as random" >> "./$folder/python/$(basename $xmlfile .xml).py"
+    echo "anonymous_student_id='abcdef1234567890'" >> "./$folder/python/$(basename $xmlfile .xml).py"
     sed -n '/^.*<script.*python.*$/,/^.*<\/script>.*$/p' $xmlfile | sed '1d;$d' >> "./$folder/python/$(basename $xmlfile .xml).py"
     sed -i 's/&amp;/\&/g' "./$folder/python/$(basename $xmlfile .xml).py"
     sed -i 's/&lt;/</g' "./$folder/python/$(basename $xmlfile .xml).py"
